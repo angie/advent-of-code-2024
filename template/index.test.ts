dd89935f78e1dcd35a06fa29a17220c0cd51fb36
Angie Merryweather <6738119+angie@users.noreply.github.com>
2024-12-04 07:08:55 +0000

    template edit

--- template/index.test.ts
+++ template/index.test.ts
@@ -4,9 +4,9 @@ import { part1, part2 } from "./index.ts";
 const testInput = ``;
 
 Deno.test("part 1: ", () => {
-  assertEquals(part1(testInput), true);
+  assertEquals(part1(testInput), 0);
 });
 
-Deno.test("part 2: ", () => {
-  assertEquals(part2(testInput), true);
+Deno.test.ignore("part 2: ", () => {
+  assertEquals(part2(testInput), 0);
 });
